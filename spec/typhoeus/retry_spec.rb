f8ba7c128d339e28864a419090442e16ade68a23
Larry Baltz <larry@baltz.org>
2012-11-16 12:14:32 +0100

    fix failing tests

--- spec/typhoeus/retry_spec.rb
+++ spec/typhoeus/retry_spec.rb
@@ -5,9 +5,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 # ruby spec/servers/app.rb -p 3001
 # ruby spec/servers/app.rb -p 3002
 
-# FIXME: why does the test server hang on PUTs?
-# def all_methods;       [:get, :head, :post, :put, :delete]; end
-def all_methods;       [:get, :head, :post, :delete]; end
+def all_methods;       [:get, :head, :post, :put, :delete]; end
 def retryable_methods; [:get]; end
 def oneshot_methods;   all_methods - retryable_methods; end
 
@@ -31,23 +29,28 @@ describe Typhoeus::Hydra do
     describe 'server sanity check' do
       describe '/flaky' do
         it "should return 503 and then 200 for too successive GETs" do
-          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-flip", :retry => false)
+          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-flip",
+                                           :retry => false)
           response.code.should == 503
 
-          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-flip", :retry => false)
+          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-flip",
+                                           :retry => false)
           response.code.should == 200
         end
 
         it "it should return 503 multiple times if needed" do
           Typhoeus::Request.get("#{@flaky_prefix}/set?codes=503,503,200")
 
-          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-list", :retry => false)
+          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-list",
+                                           :retry => false)
           response.code.should == 503
 
-          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-list", :retry => false)
+          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-list",
+                                           :retry => false)
           response.code.should == 503
 
-          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-list", :retry => false)
+          response = Typhoeus::Request.get("#{@flaky_prefix}?sanity-server-list",
+                                           :retry => false)
           response.code.should == 200
         end
       end
@@ -59,7 +62,8 @@ describe Typhoeus::Hydra do
           [503, 504].each do |code|
             it "of type #{method} with #{code} should retry request" do
               Typhoeus::Request.get("#{@flaky_prefix}/set?codes=#{code},200")
-              request = Typhoeus::Request.new("#{@flaky_prefix}?single-#{code}-success", :method => method)
+              request = Typhoeus::Request.new("#{@flaky_prefix}?single-#{code}-success",
+                                              :method => method)
               @hydra.queue request
               @hydra.run
 
@@ -69,7 +73,8 @@ describe Typhoeus::Hydra do
             it "should result in exception with multiple #{method} yielding #{code} responses" do
               Typhoeus::Request.send(method, "#{@flaky_prefix}/set?codes=#{code},#{code},200")
 
-              request = Typhoeus::Request.new("#{@flaky_prefix}?#{code}-multiple-times-fails", :method => method)
+              request = Typhoeus::Request.new("#{@flaky_prefix}?#{code}-multiple-times-fails",
+                                              :method => method)
               @hydra.queue request
 
               @hydra.run
@@ -81,7 +86,6 @@ describe Typhoeus::Hydra do
       end
 
       # TODO: check that 503 is not cached
-      # TODO: check there is no retry on POST
       # TODO: maybe check that there is log output
     end
 
@@ -90,11 +94,14 @@ describe Typhoeus::Hydra do
         retryable_methods.each do |method|
           [503, 504].each do |code|
             it "#{method} with a single #{code} should be retried (#{memoization_state})" do
+              Typhoeus::Request.get("#{@flaky_prefix}/set?codes=503,200")
               @hydra.send(memoization_state)
               callback_count = 0
-              request1 = Typhoeus::Request.new("#{@flaky_prefix}?multi-request", :method => method)
+              request1 = Typhoeus::Request.new("#{@flaky_prefix}?multi-request",
+                                               :method => method)
               request1.on_complete { callback_count += 1 }
-              request2 = Typhoeus::Request.new("#{@flaky_prefix}?multi-request", :method => method)
+              request2 = Typhoeus::Request.new("#{@flaky_prefix}?multi-request",
+                                               :method => method)
               request2.on_complete { callback_count += 1 }
               @hydra.queue request1
               @hydra.queue request2
@@ -115,9 +122,10 @@ describe Typhoeus::Hydra do
             it "#{method} with #{code} response should not be retried (#{memoization_state})" do
               @hydra.send(memoization_state)
               Typhoeus::Request.get("#{@flaky_prefix}/set?codes=#{code},200")
+              # FIXME: why does the test server hang on PUTs with body content?
               request = Typhoeus::Request.new("#{@flaky_prefix}?multi-#{code}-request",
-                                              :method => method,
-                                              :body => '[1, 2, 3]')
+                                              :params => {:q => "hi"},
+                                              :method => method)
               @hydra.queue request
 
               @hydra.run
